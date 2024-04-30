6d8e45c3b2677442a14b418c54447413b616ad82
Mullerino1 <martinemu03@gmail.com>
2024-04-30 15:14:07 +0200

    welp, it works :D

--- backend/schemaTypes/kategori.js
+++ backend/schemaTypes/kategori.js
@@ -1,4 +1,4 @@
-export default {
+export const kategori = {
     name: 'kategori',
     title: 'kategori',
     type: 'document',
--- backend/schemaTypes/schema.js
+++ backend/schemaTypes/schema.js
@@ -1,10 +1,10 @@
-import createSchema from 'part:@sanity/base/schema-creator'
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-import kategori from './kategori'
+// import createSchema from 'part:@sanity/base/schema-creator'
+// import schemaTypes from 'all:part:@sanity/base/schema-type'
+// import kategori from './kategori'
 
-export default createSchema({
-    name: 'default',
-    types: schemaTypes.concat([
-        kategori,
-    ]),
-})
+// export default createSchema({
+//     name: 'default',
+//     types: schemaTypes.concat([
+//         kategori,
+//     ]),
+// })
