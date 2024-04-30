cd7c0753c1cc6d838a6fcae8ea2338d6fd711dc3
mkihl98 <martinki@hiof.no>
2024-04-30 13:58:01 +0200

    Opprettet schema
    Forsøk på å koble react og sanity sammen/legge til schema i sanity. Foreløpig ikke stor suksess

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -4,7 +4,7 @@ const client = createClient({
     projectId: "o9tavwx2",
     dataset: "production",
     useCdn: false,
-    apiVersion: "2024-04-30",
+    apiVersion: "2022-03-07",
 })
 
 export default client
