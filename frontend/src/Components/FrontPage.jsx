c67c99d45d0d526867127fd3213bb6a61b685660
Mullerino1 <martinemu03@gmail.com>
2024-05-01 12:58:37 +0200

    Frontpage
    added into in front page

---
+++ frontend/src/Components/FrontPage.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom"
+import { fetchAllMovies } from "..sanity/movieServices"
+import { useEffect, useState } from "react"
+
+export default function FrontPage(){
+    const [movie, setMovie] = useState(null)
+
+    async function getMovies(){
+        const data = await fetchAllMovies()
+        setShows(data)
+    }
+}
+
+useEffect(() => {
+    getMovies()
+}, [])
+
+console.log(movies)
+
+return(
+    <>
+    <h1>Frontpage</h1>
+    <ul>
+        {movies?.map((movie, index) => <li key={index}>
+            <Link to={movie._id}>{movie.title}</Link>
+        </li>)}
+    </ul>
+    </>
+)
