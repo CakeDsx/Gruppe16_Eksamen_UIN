27ff8147d1a4aebde4250bfa0345141ee9901d11
Mullerino1 <martinemu03@gmail.com>
2024-05-01 15:30:13 +0200

    Update sanity.cli.js

--- backend/sanity.cli.js
+++ backend/sanity.cli.js
@@ -4,16 +4,17 @@ export default defineCliConfig({
   api: {
     projectId: 'o9tavwx2',
     dataset: 'production'
-  },
-  server:{
-    hostname: "localhost",
-    port: 3333,
-  },
-  graphql: [{
-    tag: "default",
-    playground: true,
-    generation: "gen3",
-    nonNullDocumentFields: false,
-  }],
-  vite: (config) => config,
+  }
+  // ,
+  // server:{
+  //   hostname: "localhost",
+  //   port: 3333,
+  // },
+  // graphql: [{
+  //   tag: "default",
+  //   playground: true,
+  //   generation: "gen3",
+  //   nonNullDocumentFields: false,
+  // }],
+  // vite: (config) => config,
 })
