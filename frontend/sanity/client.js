dfbdbb71fca1d7b0e654cd622779478242ddad6b
Mullerino1 <martinemu03@gmail.com>
2024-05-01 12:35:01 +0200

    more testing

--- frontend/sanity/client.js
+++ frontend/sanity/client.js
@@ -1,10 +1,14 @@
 import { createClient } from "@sanity/client"
 
-const client = createClient({
+export const client = createClient({
     projectId: "o9tavwx2",
     dataset: "production",
     useCdn: false,
     apiVersion: "2022-03-07",
 })
 
-export default client
+export const movieClient = createClient({
+    token: "sk0EFmQ5LvIy6dAbCyLZenXHNmihZtMmVlXxPnDjWMcx8HP75BV0vwGpWgIFFBK4flk56xkPNy1KsGvCQjz8KZIxSCyK3hsqSnnhxGKUCw5QKcNBvUwg5iT9ahVAxjK7R8n350KQK8QrEyFEaw2f6LTbKxWe4rxl4zGJIB4OZQ8kYdq9wqio",
+    projectId: "o9tavwx2",
+    dataset: "production",
+})
--- frontend/sanity/services/movieServices.js
+++ frontend/sanity/services/movieServices.js
@@ -1,8 +1,17 @@
 import { client } from "../client"
 
-export async function fetchMovies() {
-    const data = await client.fetch(`*[_type == "movies"]{
-        //sette inn keys(?) her?
-    }`)
+// export async function fetchMovies() {
+//     const data = await client.fetch(`*[_type == "movies"]{
+//         //sette inn keys(?) her?
+//     }`)
+//     return data
+// }
+
+export const fetchAllMovies = async () => {
+    const data = await client.fetch(`*[_type == "movie"]`)
     return data
+}
+
+export const fetchAllMovies(id) = async () ?> {
+    const data = await client.fetch(`*[_type == "movie" && -id == $id]{_id, title, reviews}`, {id})
 }
