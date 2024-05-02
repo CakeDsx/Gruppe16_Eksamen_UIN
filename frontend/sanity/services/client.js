5efb08c0db55024c24b7ffa4fe108e59f9d5b3aa
Mullerino1 <martinemu03@gmail.com>
2024-05-02 16:10:09 +0200

    its not whining anymore :D

--- frontend/sanity/services/client.js
+++ frontend/sanity/services/client.js
@@ -11,4 +11,6 @@ export const movieClient = createClient({
     token: "sk0EFmQ5LvIy6dAbCyLZenXHNmihZtMmVlXxPnDjWMcx8HP75BV0vwGpWgIFFBK4flk56xkPNy1KsGvCQjz8KZIxSCyK3hsqSnnhxGKUCw5QKcNBvUwg5iT9ahVAxjK7R8n350KQK8QrEyFEaw2f6LTbKxWe4rxl4zGJIB4OZQ8kYdq9wqio",
     projectId: "o9tavwx2",
     dataset: "production",
+    apiVersion: "v2022-03-07",
+
 })
