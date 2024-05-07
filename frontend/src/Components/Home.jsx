7297c0fb549d79ff0f86565d1dd76339de028bb1
CakeDsx <111729620+CakeDsx@users.noreply.github.com>
2024-05-07 13:09:13 +0200

    moved codes to the right files

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,81 +1,9 @@
-import React, { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
+import MovieCard from "./MovieCard";
 
-export default function Home() {
-    library.add(fab, fas, far)
-    const [movies, setMovies] = useState([])
-    const [error, setError] = useState(null)
-
-    useEffect(() => {
-        async function fetchMovies() {
-            try {
-                const url = 'https://moviesdatabase.p.rapidapi.com/titles/'
-                const options = {
-                    method: 'GET',
-                    headers: {
-                        'X-RapidAPI-Key': '023516fa7dmsh3db1b9729c75136p1ed7fejsn251f88436307',
-                        'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-                    }
-                };
-
-                const response = await fetch(url, options)
-                const data = await response.json()
-
-                if (data && Array.isArray(data.results)) {
-                    setMovies(data.results)
-                } else {
-                    setError("Invalid data format received from the API")
-                }
-            } catch (error) {
-                console.error("Error fetching movies:", error)
-                setError("Error fetching movies. Please try again later.")
-            }
-        }
-
-        fetchMovies()
-    }, [])
-
-    return (
-        <main>
-            <h2>Movies</h2>
-            {error ? (
-                <div>{error}</div>
-            ) : (
-
-                <section>
-                {movies?.map((movie, index) => (
-                    <article key={index}>
-                        <h3>{movie.titleText.text}</h3>
-                        {movie.primaryImage && movie.primaryImage.url && (
-                            <img src={movie.primaryImage.url} alt={movie.primaryImage.caption.plainText} />
-                            
-                        )}
-                        <Link to={`https://www.imdb.com/title/${movie.id}`} alt={movie.titleText}>{movie.titleText.text}</Link>
-
-                        <FontAwesomeIcon icon="fa-regular fa-star" />
-                        <FontAwesomeIcon icon="fa-solid fa-star" />
-                    </article>
-                ))}
-            </section>
-
-                // <div className="movie-list">
-                //     {movies.map((movie, index) => (
-                //         <div key={index}>
-                //             <h3>{movie.title}</h3>
-                //             {movie.primaryImage && movie.primaryImage.url && (
-                //                 <img src={movie.primaryImage.url} alt={movie.title} />
-                //             )}
-
-                //             {/* add more info here :D */}
-                //         </div>
-                //     ))}
-                // </div>
-            )}
-        </main>
-    );
+export default function Home(){
+    return(
+        <>
+        <MovieCard/>
+        </>
+    )
 }
