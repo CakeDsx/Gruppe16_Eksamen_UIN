7d4788da2285b4dd0c20af32b79ef9200cd7adda
Mullerino1 <martinemu03@gmail.com>
2024-05-08 12:09:51 +0200

    commented out something

--- frontend/sanity/services/movieServices.js
+++ frontend/sanity/services/movieServices.js
@@ -12,34 +12,34 @@
 
 
 
-import { client } from "./client"
+// import { client } from "./client"
 
-export async function fetchAllMovies() {
-    try {
-        const data = await client.fetch(`*[_type == "movie"]{
-            _id,
-            movietitle,
-            "movieslug": movieurl.current,
-            "image": movieimage.asset->url
-        }`)
-        return data
-    } catch (error) {
-        console.error("Error fetching all movies:", error)
-        throw error
-    }
-}
+// export async function fetchAllMovies() {
+//     try {
+//         const data = await client.fetch(`*[_type == "movie"]{
+//             _id,
+//             movietitle,
+//             "movieslug": movieurl.current,
+//             "image": movieimage.asset->url
+//         }`)
+//         return data
+//     } catch (error) {
+//         console.error("Error fetching all movies:", error)
+//         throw error
+//     }
+// }
 
-export async function fetchMovieBySlug(slug) {
-    try {
-        const data = await client.fetch(`*[_type == "movie" && movieurl.current == $slug]{
-            _id,
-            movietitle,
-            description,
-            "image": movieimage.asset->url
-        }`, { slug })
-        return data[0]
-    } catch (error) {
-        console.error("Error fetching movie by slug:", error)
-        throw error
-    }
-}
+// export async function fetchMovieBySlug(slug) {
+//     try {
+//         const data = await client.fetch(`*[_type == "movie" && movieurl.current == $slug]{
+//             _id,
+//             movietitle,
+//             description,
+//             "image": movieimage.asset->url
+//         }`, { slug })
+//         return data[0]
+//     } catch (error) {
+//         console.error("Error fetching movie by slug:", error)
+//         throw error
+//     }
+// }
