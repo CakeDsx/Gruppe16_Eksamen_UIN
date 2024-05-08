01030a0fd10bd4e9dd3c938fa304d7d1a3246058
CakeDsx <111729620+CakeDsx@users.noreply.github.com>
2024-05-08 15:37:09 +0200

    putting places in to correct files

---
+++ frontend/src/Components/GenrePage.jsx
@@ -0,0 +1,8 @@
+import Genres from "./Genres"
+export default function Home(){
+    return(
+        <>
+        <Genres/>
+        </>
+    )
+}
--- frontend/src/Components/Layout.jsx
+++ frontend/src/Components/Layout.jsx
@@ -1,40 +1,41 @@
 import Title from "./Title"
 import Navbar from "./Navbar"
-import Home from "./Search"
+import Home from "./Home"
+import Genres from "./Genres"
 import React, { useState, useEffect } from "react"
 //import MovieCard from "./MovieCard"
 
-export default function Layout({children}){
-    const [searchTerm, setSearchTerm] = useState("")
-    const [searchResultsState, setSearchResultsState] = useState([])
-    const [query, setQuery] = useState("")
+export default function Layout(){
+    // const [searchTerm, setSearchTerm] = useState("")
+    // const [searchResultsState, setSearchResultsState] = useState([])
+    // const [query, setQuery] = useState("")
 
-     // query ble inspirert av det som ble gjort til timen under rick and morty oppgavene https://github.com/toremake/UIN2024_coursebase/blob/main/Category_blogg/src/App.jsx
-     const fetchData = async (query) => {
-        try{
-            const response = await fetch(`https://moviesdatabase.p.rapidapi.com/titles/'${query}'`)
-            const data = await response.json()
-            setSearchResultsState(data.docs)
-        } catch (error){
-            // console.error("Det har skjedd en feil", error)
-        }
-    }
+    //  // query ble inspirert av det som ble gjort til timen under rick and morty oppgavene https://github.com/toremake/UIN2024_coursebase/blob/main/Category_blogg/src/App.jsx
+    //  const fetchData = async (query) => {
+    //     try{
+    //         const response = await fetch(`https://moviesdatabase.p.rapidapi.com/titles/'${query}'`)
+    //         const data = await response.json()
+    //         setSearchResultsState(data.docs)
+    //     } catch (error){
+    //         // console.error("Det har skjedd en feil", error)
+    //     }
+    // }
 
-    useEffect(() => {
-        if (query) {
-            fetchData(query)
-        }
-    }, [query])
+    // useEffect(() => {
+    //     if (query) {
+    //         fetchData(query)
+    //     }
+    // }, [query])
 
-    const handleSearch = () => {
-        if (searchTerm.length >= 3) {
-            setQuery(searchTerm)
-            // gjør sånn at det må være mer enn 3 tegn/bokstaver i feltet før den søker
-        }
-    }
-    const handleInputChange = (e) => {
-        setSearchTerm(e.target.value)
-    }
+    // const handleSearch = () => {
+    //     if (searchTerm.length >= 3) {
+    //         setQuery(searchTerm)
+    //         // gjør sånn at det må være mer enn 3 tegn/bokstaver i feltet før den søker
+    //     }
+    // }
+    // const handleInputChange = (e) => {
+    //     setSearchTerm(e.target.value)
+    // }
 
     return(
         <>
@@ -42,17 +43,18 @@ export default function Layout({children}){
         <Title/>
             <h1>Movie Search</h1>
             <Navbar/>
-            <nav>
+            {/* <nav>
             <input type="text" value={searchTerm} onChange={handleInputChange} placeholder="Skriv minst tre tegn"/>
             <button onClick={handleSearch}>Søk</button>
-        </nav>
+        </nav> */}
           
         </header>
        
         <main>
              <Home />
+             <Genres/>
 
-            {children}
+            {/* {children} */}
         
         </main>
         </>
