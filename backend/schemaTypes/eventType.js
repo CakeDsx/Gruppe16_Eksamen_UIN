17fc04486592610e63671d0576f213aaa4c9ac3d
Mullerino1 <martinemu03@gmail.com>
2024-05-08 11:49:02 +0200

    testing stuff for schemas

--- backend/schemaTypes/eventType.js
+++ backend/schemaTypes/eventType.js
@@ -3,20 +3,33 @@ export default {
   title: 'Movies',
   type: 'document',
   fields: [
-    {
-      name: 'name',
-      title: 'MovieTitle',
-      type: 'string'
-    },
-    {
-      name: "User_favorited",
-      title: "favorisert",
-      type: "string"
-  },
-  {
-    name: "Genre",
-    title: "Movie_Genre",
-    type: "string"
+    
+      {name: 'name', type: 'string'},
+      {name: 'title', type: 'string'},
+      {name: 'genre', type: 'string'},
+      {name: 'users', type: 'string'},
+     {name: 'imagefile', type: 'string'}
+    
+  ]
 }
-  ],
-}
+  // {
+  //   type: 'document',
+  //   name: 'title',
+  //   fields [
+  //     {name: 'movie', type: 'string'}
+
+  //   ]
+
+  // }
+//     {
+//       name: "User_favorited",
+//       title: "favorisert",
+//       type: "string"
+//   },
+//   {
+//     name: "Genre",
+//     title: "Movie_Genre",
+//     type: "string"
+// }
+//   ],
+// }
