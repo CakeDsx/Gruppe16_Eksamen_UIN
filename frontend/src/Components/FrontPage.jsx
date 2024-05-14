b516bf301f986f38c757e68dbca598162275ac88
Mullerino1 <martinemu03@gmail.com>
2024-05-14 11:14:43 +0200

    Setting up Users Ish

--- frontend/src/Components/FrontPage.jsx
+++ frontend/src/Components/FrontPage.jsx
@@ -1,10 +1,42 @@
-import { useEffect, useState } from "react"
 import { fetchAllMovies } from "../../sanity/services/movieServices"
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 
 
 export default function FrontPage() {
     
-    const [movies, setMovies] = useState(null) 
+    // const [movies, setMovies] = useState(null) 
+    const [movies, setMovies] = useState([])
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+          try {
+            const response = await fetch('https://o9tavwx2.api.sanity.io/v1/data/query/movies', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer sk0EFmQ5LvIy6dAbCyLZenXHNmihZtMmVlXxPnDjWMcx8HP75BV0vwGpWgIFFBK4flk56xkPNy1KsGvCQjz8KZIxSCyK3hsqSnnhxGKUCw5QKcNBvUwg5iT9ahVAxjK7R8n350KQK8QrEyFEaw2f6LTbKxWe4rxl4zGJIB4OZQ8kYdq9wqio',
+              },
+              body: JSON.stringify({
+                query: '*[_type == "Users"]{title, slug, id, url}',
+              }),
+            })
+    
+            if (!response.ok) {
+              throw new Error('Network response was not ok')
+            }
+    
+            const data = await response.json()
+            setMovies(data.result)
+          } catch (error) {
+            console.error('There was a problem fetching movies:', error)
+            setError('Error fetching movies. Please try again later.')
+          }
+        }
+
+        fetchMovies()
+    }, [])
 
 
     async function getMovies() {
@@ -21,12 +53,34 @@ export default function FrontPage() {
 
     return( 
         <>
-            <h1>Frontpage</h1>
+             <div>
+      <h2>FrontPage</h2>
+      {error ? (
+        <div>{error}</div>
+      ) : (
+        <section>
+          {Users.map((Users, index) => (
+            <article key={index}>
+              <Link to={`https://www.imdb.com/title/${movie.id}`} target="_blank" alt={movie.title}>
+                {movie.url && (
+                  <img src={Users.url} alt={Users.title} />
+                )}
+                <h3>{Users.title}</h3>
+              </Link>
+            </article>
+          ))}
+        </section>
+      )}
+    </div>
             
         </>
     )
 
 }
+
+  
+
+
 
 
 
