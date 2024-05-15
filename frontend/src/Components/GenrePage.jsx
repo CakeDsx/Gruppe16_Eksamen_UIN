90409180dbb2c73dfdb9337b0d145d49dd0d5ae9
Mullerino1 <martinemu03@gmail.com>
2024-05-15 12:03:25 +0200

    gjenopprettet GenrePage

--- frontend/src/Components/GenrePage.jsx
+++ frontend/src/Components/GenrePage.jsx
@@ -1,49 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
+import React from 'react'
+import Genres from './Genres'
 
-function MovieImage() {
-  library.add(fab, fas, far)
-    const [imageUrl, setImageUrl] = useState('')
+function GenrePage() {
 
-    useEffect(() => {
-        async function fetchMovieImage() {
-            const url = 'https://moviesdatabase.p.rapidapi.com/titles/search/title/Blade%20Runner?exact=true&titleType=movie'
-            const options = {
-                method: 'GET',
-                headers: {
-                    'X-RapidAPI-Key': '9f75e199fdmsh83cedb74debc28bp168dbajsnc177f705dfed',
-                    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-                }
-            };
-
-            try {
-                const response = await fetch(url, options)
-                const data = await response.json()
-                const movie = data.results[0]
-                const imageUrl = movie.primaryImage.url
-                setImageUrl(imageUrl)
-            } catch (error) {
-                console.error('Error:', error)
-            }
-        }
-
-        fetchMovieImage()
-    }, [])
 
     return (
-        <section>
-          <article>
-            {imageUrl && <img src={imageUrl} alt="Movie" />}
-            <h3>Movie title</h3>
-            <FontAwesomeIcon icon="fa-regular fa-star" />
-            <FontAwesomeIcon icon="fa-solid fa-star" />
-         </article>
-        </section>
-    );
+        <>
+        <Genres />
+        </>
+    )
 }
 
-export default MovieImage
+export default GenrePage
