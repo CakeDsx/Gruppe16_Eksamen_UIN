834e213407e2f0e5ab267ddebbd03da1dec616c5
CakeDsx <111729620+CakeDsx@users.noreply.github.com>
2024-05-15 10:21:11 +0200

    Update MovieCard.jsx

--- frontend/src/Components/MovieCard.jsx
+++ frontend/src/Components/MovieCard.jsx
@@ -1,66 +1,111 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import React, { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
+import React, { useEffect, useState } from 'react'
 
-export default function MovieCard() {
-  library.add(fab, fas, far)
-  const [movies, setMovies] = useState([])
-  const [error, setError] = useState(null)
+function MovieImage() {
+    const [imageUrl, setImageUrl] = useState('')
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await fetch('https://o9tavwx2.api.sanity.io/v1/data/query/movies', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer sk0EFmQ5LvIy6dAbCyLZenXHNmihZtMmVlXxPnDjWMcx8HP75BV0vwGpWgIFFBK4flk56xkPNy1KsGvCQjz8KZIxSCyK3hsqSnnhxGKUCw5QKcNBvUwg5iT9ahVAxjK7R8n350KQK8QrEyFEaw2f6LTbKxWe4rxl4zGJIB4OZQ8kYdq9wqio',
-          },
-          body: JSON.stringify({
-            query: '*[_type == "movie"]{title, slug, id, url}',
-          }),
-        })
+    useEffect(() => {
+        async function fetchMovieImage() {
+            const url = 'https://moviesdatabase.p.rapidapi.com/titles/search/title/Blade%20Runner?exact=true&titleType=movie'
+            const options = {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': '9f75e199fdmsh83cedb74debc28bp168dbajsnc177f705dfed',
+                    'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+                }
+            };
 
-        if (!response.ok) {
-          throw new Error('Network response was not ok')
+            try {
+                const response = await fetch(url, options)
+                const data = await response.json()
+                const movie = data.results[0]
+                const imageUrl = movie.primaryImage.url
+                setImageUrl(imageUrl)
+            } catch (error) {
+                console.error('Error:', error)
+            }
         }
 
-        const data = await response.json()
-        setMovies(data.result)
-      } catch (error) {
-        console.error('There was a problem fetching movies:', error)
-        setError('Error fetching movies. Please try again later.')
-      }
-    }
+        fetchMovieImage()
+    }, [])
 
-    fetchMovies()
-  }, [])
-
-  return (
-    <div>
-      <h2>Movies</h2>
-      {error ? (
-        <div>{error}</div>
-      ) : (
+    return (
         <section>
-          {movies.map((movie, index) => (
-            <article key={index}>
-              <Link to={`https://www.imdb.com/title/${movie.id}`} target="_blank" alt={movie.title}>
-                {movie.url && (
-                  <img src={movie.url} alt={movie.title} />
-                )}
-                <h3>{movie.title}</h3>
-              </Link>
-              <FontAwesomeIcon icon="fa-regular fa-star" />
-              <FontAwesomeIcon icon="fa-solid fa-star" />
-            </article>
-          ))}
+          <article>
+            <h1>Movie Image</h1>
+            {imageUrl && <img src={imageUrl} alt="Movie" />}
+         </article>
         </section>
-      )}
-    </div>
-  )
+    );
 }
+
+export default MovieImage
+
+
+
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import React, { useState, useEffect } from "react"
+// import { Link } from "react-router-dom"
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// import { far } from '@fortawesome/free-regular-svg-icons'
+
+// export default function MovieCard() {
+//   library.add(fab, fas, far)
+//   const [movies, setMovies] = useState([])
+//   const [error, setError] = useState(null)
+
+//   useEffect(() => {
+//     const fetchMovies = async () => {
+//       try {
+//         const response = await fetch('https://o9tavwx2.api.sanity.io/v1/data/query/movies', {
+//           method: 'POST',
+//           headers: {
+//             'Content-Type': 'application/json',
+//             Authorization: 'Bearer sk0EFmQ5LvIy6dAbCyLZenXHNmihZtMmVlXxPnDjWMcx8HP75BV0vwGpWgIFFBK4flk56xkPNy1KsGvCQjz8KZIxSCyK3hsqSnnhxGKUCw5QKcNBvUwg5iT9ahVAxjK7R8n350KQK8QrEyFEaw2f6LTbKxWe4rxl4zGJIB4OZQ8kYdq9wqio',
+//           },
+//           body: JSON.stringify({
+//             query: '*[_type == "movie"]{title, slug, id, url}',
+//           }),
+//         })
+
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok')
+//         }
+
+//         const data = await response.json()
+//         setMovies(data.result)
+//       } catch (error) {
+//         console.error('There was a problem fetching movies:', error)
+//         setError('Error fetching movies. Please try again later.')
+//       }
+//     }
+
+//     fetchMovies()
+//   }, [])
+
+//   return (
+//     <div>
+//       <h2>Movies</h2>
+//       {error ? (
+//         <div>{error}</div>
+//       ) : (
+//         <section>
+//           {movies.map((movie, index) => (
+//             <article key={index}>
+//               <Link to={`https://www.imdb.com/title/${movie.id}`} target="_blank" alt={movie.title}>
+//                 {movie.url && (
+//                   <img src={movie.url} alt={movie.title} />
+//                 )}
+//                 <h3>{movie.title}</h3>
+//               </Link>
+//               <FontAwesomeIcon icon="fa-regular fa-star" />
+//               <FontAwesomeIcon icon="fa-solid fa-star" />
+//             </article>
+//           ))}
+//         </section>
+//       )}
+//     </div>
+//   )
+// }
