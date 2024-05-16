131cbf02ef709ccbf09c03b59555289a1f3bee00
CakeDsx <111729620+CakeDsx@users.noreply.github.com>
2024-05-16 15:37:48 +0200

    yippiiiee

--- frontend/src/Components/Genres.jsx
+++ frontend/src/Components/Genres.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
+import { faStar as fas, faStar as far } from '@fortawesome/free-solid-svg-icons'
+import { faStar as fab } from '@fortawesome/free-regular-svg-icons'
 
 export default function Genres(){
     library.add(fab, fas, far)
     const [genres, setGenres] = useState([])
     const [error, setError] = useState(null)
+    const [isStarSolid, setisStarSolid] = useState([])
+
 
     useEffect(() => {
         async function fetchGenres() {
@@ -16,11 +17,11 @@ export default function Genres(){
 
     const url = 'https://moviesdatabase.p.rapidapi.com/titles/utils/genres'
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '023516fa7dmsh3db1b9729c75136p1ed7fejsn251f88436307',
-		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '023516fa7dmsh3db1b9729c75136p1ed7fejsn251f88436307',
+        'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+    }
 }
 
 const response = await fetch(url, options)
@@ -37,21 +38,30 @@ const response = await fetch(url, options)
             }
         }
 
+        setisStarSolid(new Array(genres.length).fill(false))
+
+
         fetchGenres()
     }, [])
+
+    const toggleStar = (index) => {
+        const updatedStars = [...isStarSolid]
+        updatedStars[index] = !updatedStars[index]
+        setisStarSolid(updatedStars)
+      }
     
     return (
         <>
             <ul>
-                <h2>Genres</h2>
                 {genres
                     ? genres
                         .filter((genres, index) => index > 0)
                         .map((genres, index) => (
                             <li key={index}>
                                 <span>{index + 1}. {genres}</span>
-                                <FontAwesomeIcon icon={['far', 'star']} />
-                                <FontAwesomeIcon icon={['fas', 'star']} />
+                                <button onClick={() => toggleStar (index)}>
+              <FontAwesomeIcon icon={isStarSolid[index] ? fas : fab} />
+              </button>
                             </li>
                         ))
                     : null
